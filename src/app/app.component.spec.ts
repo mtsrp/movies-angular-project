dfcdf6765d15a15a425a20db74b19e20471380cf
mtsrp <30304075+mtsrp@users.noreply.github.com>
2022-10-16 20:40:30 +0100

    secao 5 - application shell

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'movies-angular-project'`, () => {
+  it(`should have as title 'movies-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('movies-angular-project');
+    expect(app.title).toEqual('movies-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('movies-angular-project app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('movies-app app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'movies-angular-project';
+  title = 'movies-app';
 }
