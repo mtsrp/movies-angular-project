b917785ac20a2b49851998cca464dcf8819b16cb
mtsrp <30304075+mtsrp@users.noreply.github.com>
2022-10-17 23:11:16 +0100

    Define Movie Model

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from '../../models/movie';
 
 @Component({
   selector: 'slider',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
 
+  @Input() items: Movie[] = [null];
   constructor() { }
 
   ngOnInit(): void {
